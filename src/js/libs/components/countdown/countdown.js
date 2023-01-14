189016c7191fc0373b85c2b73408c3d043e2c132
luigibreda <luigimatheus@hotmail.com>
2023-01-13 21:37:15 -0300

    Modificaçõe Tarek

--- src/js/libs/components/countdown/countdown.js
+++ src/js/libs/components/countdown/countdown.js
@@ -2,7 +2,7 @@ export function initCountdown() {
   return {
     setupCountdown() {
       // Set the date we're counting down to
-      var countDownDate = new Date("Jan 7, 2023 00:00:00").getTime();
+      var countDownDate = new Date("Jan 21, 2023 20:00:00").getTime();
 
 
       // Update the count down every 1 second
